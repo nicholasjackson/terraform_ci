3db4bd1f389c6f42434b228ef016fe230dc86e2f
Nic Jackson <jackson.nic@gmail.com>
2018-04-22 17:08:12 +0100

    Updated

--- terraform.tf
+++ terraform.tf
@@ -17,7 +17,7 @@ terraform {
 
 resource "digitalocean_droplet" "web" {
   image  = "ubuntu-14-04-x64"
-  name   = "web-1"
+  name   = "${terraform.workspace}-web-1"
   region = "nyc3"
   size   = "512mb"
 }
